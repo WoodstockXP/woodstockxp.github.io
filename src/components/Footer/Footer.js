d95c0a1b676c052f40262d83b35a86f0df17f712
Woodstock <emil.moreno2002@gmail.com>
2024-11-10 16:59:41 -0600

    Minor fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/WoodstockXP'
       className='link footer__link'
     >
       /goodbye world
